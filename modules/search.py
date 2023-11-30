fc9b8e684741d285f6bfa2a5f0cfa9ac18430a4f
KC Flynn <kc@flynnkc.com>
2023-11-29 22:35:12 -0500

    Trimmed packages; Added to search

--- modules/search.py
+++ modules/search.py
@@ -3,6 +3,7 @@
 import logging
 
 from modules.config import Config
+from oci import pagination
 from oci.resource_search import ResourceSearchClient
 from oci.resource_search.models import StructuredSearchDetails
 
@@ -43,14 +44,24 @@ class Search:
 
     def __init__(self, config: Config):
         self.log = logging.getLogger(f'{__name__}.Search')
-        self.log.debug('Initializing Search Object')
+        self.log.info('Initializing Search Object')
         self.client = ResourceSearchClient(config.config, signer=config.signer)
         self.log.debug(f'Search client: {self.client}')
 
         # Create structured search with parameters
-        split = ", "
-        query = f"query {split.join(self.search_types)} resources where (definedTags.namespace = '{config.args.tag}')"
-        query += f" && compartmentId  = '{config.args.compartment}'" if config.args.compartment else ""
-        query += f" && compartmentId  != '{config.args.exclude}'" if config.args.exclude else ""
+        comma = ", "
+        query = (f"query {comma.join(self.search_types)} resources where"
+                 f" (definedTags.namespace = '{config.args.tag}')")
+        query += f" && compartmentId  = '{config.args.compartment}'" if \
+            config.args.compartment else ""
+        query += f" && compartmentId  != '{config.args.exclude}'" if \
+            config.args.exclude else ""
 
         details = StructuredSearchDetails(query=query)
+        response = pagination.list_call_get_all_results(self.client.search_resources, details)
+        self.log.debug(f'Response Headers: {response.headers}\n\t'
+                       f'Request ID: {response.request_id}\n\tRequest: '
+                       f'{response.request}\n\tStatus: {response.status}')
+        self.data = response.data.items
+        self.log.debug(f'Initialized Search with data: {self.data}')
+        self.log.info(f'Found {len(self.data)} Resources')
